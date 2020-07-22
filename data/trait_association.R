697e33b3ea0d8a9c5bbd35593f92e79e6005bb8b
Michael Scherer <s9mlscer@gmail.com>
2020-07-22 11:17:50 +0200

    Small updates on the protocols and website in general

--- data/trait_association.R
+++ data/trait_association.R
@@ -53,7 +53,7 @@ cors <- apply(props,1,function(x){
     §cor.test(x[!na.trait],as.numeric(trait[!na.trait]))$p.value
   })
 })
-corrplot(t(cors),"ellipse",addCoef.col = "black"col=rev(colorRampPalette(c("#67001F", 
+corrplot(t(cors),"ellipse",addCoef.col = "black",col=rev(colorRampPalette(c("#67001F",
 				"#B2182B", "#D6604D", "#F4A582",
                                 "#FDDBC7", "#FFFFFF", "#D1E5F0", "#92C5DE",
                                 "#4393C3", "#2166AC", "#053061"))(200)))
